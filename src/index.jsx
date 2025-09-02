407e7d575fe9e15b0d8ea4a3b2668f4ebc5977d1
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-09-02 10:28:27 +0200

    p1c2

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './pages/Home'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // ⚠️ important : 'react-dom/client' pour React 18
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
+import Header from './components/Header';
+import Error from './components/Error';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // ⚡ créer le root
+root.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
